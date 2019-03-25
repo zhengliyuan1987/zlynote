864be35a5b3ca173f87f8e6464a2b503c657635b
Yuanqiang Li <yuanqian@xilinx.com>
2019-03-25 00:08:56 -0600

    implement n streams to one based on round_robin

--- l1_module/include/xf_util/stream_n_to_one/round_robin.h
+++ l1_module/include/xf_util/stream_n_to_one/round_robin.h
@@ -3,13 +3,21 @@
 
 #include "xf_util/types.h"
 #include "xf_util/enums.h"
+#include "xf_util/common.h"
 
 // Forward decl
 
 namespace xf {
 namespace util {
 namespace level1 {
-
+/*
+ *
+ *
+ *
+ *
+ *
+ * 
+ * */
 template <int _WInStrm, int _WOutStrm, int _NStrm>
 void strm_n_to_one(hls::stream<ap_uint<_WInStrm> > istrms[_NStrm],
                    hls::stream<bool> e_istrms[_NStrm],
@@ -31,4 +39,244 @@ void strm_n_to_one(hls::stream<_TIn> istrms[_NStrm],
 // Implementation
 // TODO
 
+
+namespace xf {
+namespace util {
+namespace level1 {
+
+namespace details {
+
+///////////////////////////////////
+template <int _WInStrm, int _WOutStrm, int _NStrm>
+void strm_n_to_one_read(hls::stream<ap_uint<_WInStrm> > istrms[_NStrm],
+                   hls::stream<bool> e_istrms[_NStrm],
+                   hls::stream<ap_uint<32> > &left_n,
+                   hls::stream<ap_uint<_WInStrm*_NStrm> >& n_in_strm,
+                   hls::stream<bool>& e_n_in_strm) {
+  ap_uint<_NStrm> ends=0;
+  ap_uint<_WInStrm*_NStrm> cmb=0;
+  for(int id=0; id< _NStrm; ++id) {    
+   #pragma HLS unroll
+   ends[id] = e_istrms[id].read();
+  }
+ // read _NStrm data from input streams at the  same time, then output them
+  while(ends == 0 ) {
+    #pragma HLS pipeline II=1 
+      for(int id=0; id< _NStrm; ++id) {    
+         #pragma HLS unroll
+           ap_uint<_WInStrm> d = istrms[id].read();
+           cmb.range((id+1)*_WInStrm-1, id * _WInStrm)=d;           
+           ends[id] = e_istrms[id].read();
+        }
+        #if !defined(__SYNTHESIS__) && XF_UTIL_STRM_1NRR_DEBUG == 1
+           std::cout<< "comb="<<cmb<<std::endl;
+         #endif
+       n_in_strm.write(cmb);
+       e_n_in_strm.write(false);
+  }
+  // read data from unfinished streams 
+  int left=_NStrm;
+  for(int id=0; id< _NStrm; ++id) {    
+       if(!ends[id]){  
+         ap_uint<_WInStrm> d = istrms[id].read();
+         cmb.range((id+1)*_WInStrm-1, id * _WInStrm)=d;           
+         ends[id] = e_istrms[id].read();
+         left--;
+        }
+  }
+ // output the data from last loop  
+ if( left < _NStrm)  {
+       n_in_strm.write(cmb);
+       e_n_in_strm.write(false);
+  }
+      #if !defined(__SYNTHESIS__) && XF_UTIL_STRM_1NRR_DEBUG == 1
+         std::cout<< "comb="<<cmb<<std::endl;
+       #endif
+  // how many data are available in the last loop
+  left_n.write(left<_NStrm? left:0);
+  
+  e_n_in_strm.write(true);
+
+}
+
+template <int _WInStrm, int _WOutStrm,int _NStrm>
+void strm_n_to_one_collect(
+                   hls::stream<ap_uint<_WInStrm*_NStrm> > &n_in_strm,
+                   hls::stream<bool> &e_n_in_strm,
+                   hls::stream<ap_uint<32> > &left_n,
+                   hls::stream<ap_uint<32> > &left_b,
+                   hls::stream<ap_uint< lcm<_WInStrm*_NStrm, _WOutStrm>::value> >& buf_strm,
+                   hls::stream<bool>& e_buf_strm) {
+  const int buf_size  = lcm<_WInStrm*_NStrm, _WOutStrm>::value;
+  const int num_in    = buf_size/_WInStrm;
+  const int count_in  = num_in/_NStrm;
+  int p=0;
+  ap_uint<buf_size> buf_a;
+  int pos=0;
+  bool last= e_n_in_strm.read();
+  while(!last)  {
+    #pragma HLS pipeline II=1 
+    int low=pos;
+    pos += _NStrm*_WInStrm;
+    buf_a.range(pos-1,low)=n_in_strm.read();    
+    last = e_n_in_strm.read();
+    if(!last && p+1== count_in) {
+      e_buf_strm.write(false);
+      buf_strm.write(buf_a);
+      p   = 0;
+      pos = 0;
+      #if !defined(__SYNTHESIS__) && XF_UTIL_STRM_1NRR_DEBUG == 1
+           std::cout<< "buf_a="<<buf_a<<std::endl;
+      #endif
+     }
+    else
+    p++;
+  }
+    buf_strm.write(buf_a);
+    e_buf_strm.write(true); // even if true, the data in buf_strm is available.
+ 
+   int left=left_n.read();
+   if (left >0)
+     left_b.write(pos-(_NStrm-left)*_WInStrm);
+   else
+     left_b.write(pos);
+}
+
+
+template <int _WInStrm, int _WOutStrm, int _NStrm>
+void strm_n_to_one_distribute(
+                   hls::stream<ap_uint< lcm<_WInStrm*_NStrm, _WOutStrm>::value> >& buf_strm,
+                   hls::stream<bool> &e_buf_strm,
+                   hls::stream<ap_uint<32> > &left_b,
+                   hls::stream<ap_uint<_WOutStrm> >& ostrm,
+                   hls::stream<bool>& e_ostrm) {
+  const int buf_size = lcm<_WInStrm*_NStrm, _WOutStrm>::value;
+  const int num_out = buf_size/_WOutStrm;
+
+  ap_uint<buf_size> buf_b;
+  int low   = 0;
+  int up    = 0;
+  int c     = num_out;
+  bool last = false; 
+  unsigned int up_pos= -1;  
+  while(!last) {
+   #pragma HLS pipeline II=1 
+    if(c==num_out) {
+        buf_b= buf_strm.read();    
+        c=0;
+        last= e_buf_strm.read(); 
+        if (last)
+         up_pos=left_b.read();
+     }
+    if( (c+1)*_WOutStrm< up_pos) {
+       ostrm.write(buf_b.range((c+1)*_WOutStrm-1, c*_WOutStrm));
+       e_ostrm.write(false);
+     }
+      c++;
+  }
+ e_ostrm.write(true);
+
+}
+
+//////////////////////////////////////////////////
+
+template <int _WInStrm, int _WOutStrm, int _NStrm>
+void strm_n_to_one_round_robin(hls::stream<ap_uint<_WInStrm> > istrms[_NStrm],
+                   hls::stream<bool> e_istrms[_NStrm],
+                   hls::stream<ap_uint<_WOutStrm> >& ostrm,
+                   hls::stream<bool>& e_ostrm) {
+  
+const int buf_size = lcm<_WInStrm*_NStrm, _WOutStrm>::value;
+hls::stream<ap_uint<_WInStrm*_NStrm> > n_in_strm;
+#pragma HLS stream variable = n_in_strm depth = 8
+hls::stream<bool> e_n_in_strm;
+#pragma HLS stream variable = e_n_in_strm depth = 8
+hls::stream<ap_uint<buf_size> > buf_strm;
+#pragma HLS stream variable = buf_strm depth = 8
+hls::stream<bool> e_buf_strm;
+#pragma HLS stream variable = e_buf_strm depth = 8
+hls::stream<ap_uint<32> > left_n; //how many input(_WInStrm bits) are stored in  last data(_NStrm*WInstrm bits) in n_in_strm
+hls::stream<ap_uint<32> > left_b; // how many input(_WInStrm bits) are stored in last data(buf_size bits) in buf_strm
+ 
+#pragma HLS dataflow
+
+/*  read data   : read data from input streams, output _NStrm * _WInStrm bits
+ *  collect data: buffer  buf_size=lcm<_WInStrm*_NStrm, _WOutStrm> bits and output it.
+ *  distribute  : output buf_size/_WOutStrm data when read buf_size once
+ *
+ * */
+ 
+strm_n_to_one_read < _WInStrm,_WOutStrm, _NStrm>
+                ( istrms,
+                  e_istrms,
+                  left_n,
+                  n_in_strm,
+                  e_n_in_strm);
+
+strm_n_to_one_collect < _WInStrm, _WOutStrm, _NStrm>
+                (  n_in_strm,
+                   e_n_in_strm,
+                   left_n,
+                   left_b,
+                   buf_strm,
+                   e_buf_strm);
+
+
+strm_n_to_one_distribute < _WInStrm,  _WOutStrm, _NStrm>
+               ( buf_strm,
+                 e_buf_strm,
+                 left_b,
+                 ostrm,
+                 e_ostrm);
+}
+
+
+} // details
+
+template <int _WInStrm, int _WOutStrm, int _NStrm>
+void strm_n_to_one(hls::stream<ap_uint<_WInStrm> > istrms[_NStrm],
+                   hls::stream<bool> e_istrms[_NStrm],
+                   hls::stream<ap_uint<_WOutStrm> >& ostrm,
+                   hls::stream<bool>& e_ostrm,
+                   round_robin_t _op) {
+   
+    details::strm_n_to_one_round_robin<_WInStrm, _WOutStrm, _NStrm>( istrms,
+                                       e_istrms,
+                                       ostrm,
+                                       e_ostrm);
+
+}
+
+
+//------------------------------------------------------------------------------//
+namespace details {
+template <typename _TIn, int _NStrm>
+void strm_n_to_one_round_robin_type(hls::stream<_TIn> istrms[_NStrm],
+                   hls::stream<bool> e_istrms[_NStrm],
+                   hls::stream<_TIn>& ostrm,
+                   hls::stream<bool>& e_ostrm) {
+  
+ 
+
+}
+
+} // details
+
+template <typename _TIn, int _NStrm>
+void strm_n_to_one(hls::stream<_TIn> istrms[_NStrm],
+                   hls::stream<bool> e_istrms[_NStrm],
+                   hls::stream<_TIn>& ostrm,
+                   hls::stream<bool>& e_ostrm,
+                   round_robin_t _op) {
+// TODO
+    details::strm_n_to_one_round_robin_type<_TIn, _NStrm>( istrms,
+                                       e_istrms,
+                                       ostrm,
+                                       e_ostrm);
+
+}
+
+} // level1
+} // util
+} // xf
 #endif // XF_UTIL_STREAM_N1_RR
---
+++ l1_module/tests/stream_n_to_one/round_robin/test.cpp
@@ -0,0 +1,113 @@
+
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+
+
+#include "xf_util/stream_n_to_one/round_robin.h"
+
+#define WIN_STRM  16 
+#define WOUT_STRM 256
+#define NS       1024*8
+#define NSTRM    16
+
+
+void test_core_n_1(hls::stream<ap_uint<WIN_STRM> > istrms[NSTRM],
+                   hls::stream<bool> e_istrms[NSTRM],
+                   hls::stream<ap_uint<WOUT_STRM> >& ostrm,
+                   hls::stream<bool>& e_ostrm
+                   ) {
+  xf::util::level1::strm_n_to_one<WIN_STRM, WOUT_STRM,NSTRM>(
+                        istrms, e_istrms,
+                        ostrm, e_ostrm,
+                        xf::util::round_robin_t());
+                        
+}
+
+
+int test_n_1(){
+
+   hls::stream<ap_uint<WIN_STRM> > data_istrms[NSTRM];
+   hls::stream<bool> e_data_istrms[NSTRM];
+   hls::stream<ap_uint<WOUT_STRM> > data_ostrm;
+   hls::stream<bool> e_data_ostrm;
+   
+  const int buf_width = xf::util::lcm<WIN_STRM * NSTRM, WOUT_STRM>::value;
+  const int num_in    = buf_width/WIN_STRM;
+  const int num_out   = buf_width/WOUT_STRM;
+
+  ap_uint<buf_width> buff_test=0;
+  ap_uint<buf_width> buff_gld=0;
+  std::cout<<std::dec<< "WIN_STRM  = "<< WIN_STRM <<std::endl;
+  std::cout<<std::dec<< "WOUT_STRM = "<< WOUT_STRM <<std::endl;
+  std::cout<<std::dec<< "NSTRM     = "<< NSTRM <<std::endl;
+  std::cout<<std::dec<< "NS        = "<< NS <<std::endl;
+  for(int d=0; d< NS; ++d)
+  { 
+    ap_uint<WIN_STRM> id =d % NSTRM; 
+    data_istrms[id].write(d);
+    e_data_istrms[id].write(false);
+#if !defined(__SYNTHESIS__) && XF_UTIL_STRM_1NRR_DEBUG == 1
+    std::cout<< "id = "<< id << "  "<< "d="<<d << std::endl;
+#endif
+  }
+
+ for(int i=0; i<NSTRM; ++i)
+   e_data_istrms[i].write(true);
+
+  test_core_n_1( data_istrms, e_data_istrms,
+                   data_ostrm, e_data_ostrm);
+ int nerror = 0;
+ int n      = 0;
+ int k      = 0;
+ bool last= e_data_ostrm.read();
+ while(!last) {
+   // get num_out*WOUT_STRM bits from stream 
+   while(!last && k< num_out){
+    last= e_data_ostrm.read();
+    ap_uint<WOUT_STRM> d = data_ostrm.read();
+    buff_test.range( (k+1)*WOUT_STRM-1, k*WOUT_STRM)= d;
+    ++k;
+   }
+   if(k == num_out)  {
+     k=0;
+     //get num_in*WIN_STRM bits from golden data;
+     for(int i=0; i<num_in ; ++i){
+      buff_gld.range( (i+1)*WIN_STRM-1, i*WIN_STRM)= n;
+      ++n; 
+     }
+     // compare (num_in*WIN_STRM = num_out*WOUT_STRM) bits 
+     if ( buff_test != buff_gld) {
+      nerror=1;
+      std::cout<< "buff_test = "<< buff_test << std::endl;
+      std::cout<< "buff_gld  = "<< buff_gld << std::endl;
+     }
+   } //if (k == num_out)
+ } // while(!last)
+ if (k>0) {
+    for(int i=0; i<num_in ; ++i) {
+     buff_gld.range( (i+1)*WIN_STRM-1, i*WIN_STRM)= n;
+     ++n; 
+    }
+    int up= k* WOUT_STRM-1;
+    if ( buff_test.range(up,0) != buff_gld.range(up,0))  {
+     nerror=1;
+     std::cout<< "buff_test = "<< buff_test << std::endl;
+     std::cout<< "buff_gld  = "<< buff_gld << std::endl;
+    }
+ }
+   if (nerror) {
+        std::cout << "\nFAIL: " << nerror << "the order is wrong.\n";
+   } 
+   else {
+        std::cout << "\nPASS: no error found.\n";
+   }
+   return nerror;
+
+}
+
+int main()
+{
+   return test_n_1(); 
+}
+
