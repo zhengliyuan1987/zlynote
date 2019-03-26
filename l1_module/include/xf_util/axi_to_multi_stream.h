e658f22b5175bdaddca07971e8451dba515c6b3d
Yuanjie Huang <yuanjieh@xilinx.com>
2019-03-26 13:53:17 +0800

    axi_to_stream: document API

---
+++ l1_module/include/xf_util/axi_to_multi_stream.h
@@ -0,0 +1,70 @@
+#ifndef XF_UTIL_AXI_TO_MULTI_STRM_H
+#define XF_UTIL_AXI_TO_MULTI_STRM_H
+
+#include "xf_util/types.h"
+#include "xf_util/enums.h"
+
+// Forward decl
+
+namespace xf {
+namespace util {
+namespace level1 {
+
+/* @brief Loading multiple categories of data from one AXI master to streams.
+ *
+ * This primitive assumes the width of AXI port is multiple of alignment width.
+ * When alignment width is less than AXI port width, the AXI port bandwidth
+ * will not be fully used.
+ *
+ * AXI port width and width of each type are assumed to be multiple of 8.
+ * It is assumed that the data width in bits is ``8 * sizeof(T)``, and data
+ * type can be casted from raw bits of matching width.
+ *
+ * This module assumes the data is tightly packed, so that the begining of
+ * Type 2 data may be placed in one AXI port row with the end of Type 1 data.
+ *
+ * \rst
+ * ::
+ *
+ *     AXI word [ elements of Type 1 ........................................ ]
+ *     AXI word [ elements of Type 1 ..... end | begin elements of Type 2 ... ]
+ *     AXI word [ elements of Type 2 ........................................ ]
+ *
+ * \endrst
+ *
+ * @tparam _WAxi width of AXI port, must be power of 2 and between 8 to 512.
+ * @tparam _WAlign data's alignment width in buffer.
+ * @tparam _TStrm data's type.
+ *
+ * @param rbuf input AXI port.
+ * @param ostrm1 output stream of type 1.
+ * @param e_ostrm1 end flag for output stream of type 1.
+ * @param ostrm2 output stream of type 2.
+ * @param e_ostrm2 end flag for output stream of type 2.
+ * @param ostrm3 output stream of type 3.
+ * @param e_ostrm3 end flag for output stream of type 3.
+ * @param num number of data to load from AXI port for each type.
+ * @param offset offset for each type, in number of char.
+ */
+template <int _WAxi, typename _TStrm1, typename _TStrm2, typename _TStrm3>
+void axi_to_stream(
+    ap_uint<_WAxi>* rbuf,
+    hls::stream<_TStrm1>& ostrm1,
+    hls::stream<bool>& e_ostrm1,
+    hls::stream<_TStrm2>& ostrm2,
+    hls::stream<bool>& e_ostrm2,
+    hls::stream<_TStrm3>& ostrm3,
+    hls::stream<bool>& e_ostrm3,
+    const int num[3],
+    const int offset[3]);
+
+// TODO for 2 and for 4
+
+} // level1
+} // util
+} // xf
+
+// Implementation
+// TODO
+
+#endif // XF_UTIL_AXI_TO_MULTI_STRM_H
--- l1_module/include/xf_util/axi_to_stream.h
+++ l1_module/include/xf_util/axi_to_stream.h
@@ -10,6 +10,23 @@ namespace xf {
 namespace util {
 namespace level1 {
 
+/* @brief Loading data from AXI master to stream.
+ *
+ * This primitive assumes the width of AXI port is multiple of data width.
+ * When data width is less than AXI port width, the AXI port bandwidth
+ * will not be fully used.
+ *
+ * Both AXI port and data are assumed to have width as multiple of 8-bit char.
+ *
+ * @tparam _WAxi width of AXI port, must be power of 2 and between 8 to 512.
+ * @tparam _WStrm width of the stream.
+ *
+ * @param rbuf input AXI port.
+ * @param ostrm output stream.
+ * @param e_ostrm end flag for output stream.
+ * @param num number of data to load from AXI port.
+ * @param offset offset from the beginning of the buffer, in number of char.
+ */
 template <int _WAxi, int _WStrm>
 void axi_to_stream(
     ap_uint<_WAxi>* rbuf,
@@ -18,6 +35,25 @@ void axi_to_stream(
     const int num,
     const int offset = 0);
 
+/* @brief Loading data from AXI master to stream.
+ *
+ * This primitive assumes the width of AXI port is multiple of alignment width.
+ * When alignment width is less than AXI port width, the AXI port bandwidth
+ * will not be fully used.
+ *
+ * Both AXI port and alignment width are assumed to be multiple of 8.
+ * The data width must be no greater than its alignment width.
+ *
+ * @tparam _WAxi width of AXI port, must be power of 2 and between 8 to 512.
+ * @tparam _WAlign data's alignment width in buffer.
+ * @tparam _TStrm data's type.
+ *
+ * @param rbuf input AXI port.
+ * @param ostrm output stream.
+ * @param e_ostrm end flag for output stream.
+ * @param num number of data to load from AXI port.
+ * @param offset offset from the beginning of the buffer, in number of char.
+ */
 template <int _WAxi, int _WAlign, typename _TStrm>
 void axi_to_stream(
     ap_uint<_WAxi>* rbuf,
