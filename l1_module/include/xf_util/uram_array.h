2b41ebce7fab2a259d7472556f1211d345378f70
yuxiang <yuxiangz@xilinx.com>
2019-04-22 22:21:22 -0700

    Add calloc function for uram

--- l1_module/include/xf_util/uram_array.h
+++ l1_module/include/xf_util/uram_array.h
@@ -29,8 +29,8 @@
 #endif
 
 #include <ap_int.h>
-#include <iostream>
 #include <math.h>
+#include <iostream>
 
 namespace xf {
 namespace util {
@@ -41,17 +41,19 @@ namespace level1 {
 /// @tparam width width of one data.
 /// @tparam depth size of one array.
 /// @tparam B     condition of W <= 72.
-template <int W, int N, bool B = (W <= 72)> struct need_num {
-private:
+template <int W, int N, bool B = (W <= 72)>
+struct need_num {
+ private:
   static const int elem_per_line = 72 / W;
 
-public:
+ public:
   static const int value = ((W + 71) / 72) * ((N + (elem_per_line * 4096) - 1) /
                                               (elem_per_line * 4096));
 };
 
-template <int W, int N> struct need_num<W, N, false> {
-public:
+template <int W, int N>
+struct need_num<W, N, false> {
+ public:
   static const int value = ((W + 71) / 72) * ((N + 4095) / 4096);
 };
 
@@ -61,19 +63,25 @@ public:
 /// @tparam WData  the width of every element.
 /// @tparam NData  the depth of array.
 /// @tparam NCache the number of cache.
-template <int WData, int NData, int NCache> class uram_array {
-public:
+template <int WData, int NData, int NCache>
+class uram_array {
+ public:
   uram_array()
-      : _elem_per_line(72 / WData), _num_one_process((WData + 71) / 72),
+      : _elem_per_line(72 / WData),
+        _num_one_process((WData + 71) / 72),
         _index_one_block(_elem_per_line * 4096) {
 #pragma HLS RESOURCE variable = _blocks core = XPM_MEMORY uram
+#pragma HLS ARRAY_PARTITION variable = _blocks complete dim = 1
+#pragma HLS ARRAY_PARTITION variable = _index complete dim = 1
+#pragma HLS ARRAY_PARTITION variable = _state complete dim = 1
 #ifndef __SYNTHESIS__
     // TODO:malloc the array
     for (int i = 0; i < _num_uram_block; i++) {
-      _blocks[i] = (ap_uint<72> *)malloc(sizeof(ap_uint<72>) * 4096);
+      _blocks[i] = (ap_uint<72>*)malloc(sizeof(ap_uint<72>) * 4096);
     }
 #endif
     for (int i = 0; i < NCache; i++) {
+#pragma HLS unroll
       _index[i] = -1;
       _state[i] = 0;
     }
@@ -87,17 +95,22 @@ public:
 #endif
   }
 
+  /// @brief  initialization for uram.
+  /// @param  d value for initialization.
+  /// @return number of block which had been initialize.
+  int calloc_uram(const ap_uint<WData>& d);
+
   /// @brief write to uram.
   /// @param index the index which you want to write.
   /// @param d     the value what you want to write.
-  void write(int index, const ap_uint<WData> &d);
+  void write(int index, const ap_uint<WData>& d);
 
-  /// @brief read from uram.
-  /// @param index the index which you want to read.
+  /// @brief  read from uram.
+  /// @param  index the index which you want to read.
   /// @return value you had read.
   ap_uint<WData> read(int index);
 
-private:
+ private:
   /// number elements per line, used with WData<=72. For example, when WData =
   /// 16, _elem_per_line is 4.
   const int _elem_per_line;
@@ -117,10 +130,10 @@ private:
   /// the cache for saving latest value.
   ap_uint<WData> _state[NCache];
 
-public:
+ public:
 /// the memory of accessing,one block's fixed width is 72 and the depth is 4K.
 #ifndef __SYNTHESIS__
-  ap_uint<72> *_blocks[need_num<WData, NData>::value];
+  ap_uint<72>* _blocks[need_num<WData, NData>::value];
 #else
   ap_uint<72> _blocks[need_num<WData, NData>::value][4096];
 #endif
@@ -131,15 +144,60 @@ const int uram_array<WData, NData, NCache>::_num_uram_block =
     need_num<WData, NData>::value;
 
 template <int WData, int NData, int NCache>
+int uram_array<WData, NData, NCache>::calloc_uram(const ap_uint<WData>& d) {
+  if (_num_uram_block == 0) return 0;
+
+  ap_uint<72> t;
+  if (WData <= 72) {
+  init_loop:
+    for (int i = 0; i < 4096; i++) {
+#pragma HLS PIPELINE II = 1
+    init_one_line:
+      for (int nb = 0; nb < _num_uram_block; nb++) {
+#pragma HLS inline
+      init_per_72:
+        for (int j = 0; j < _elem_per_line; j++) {
+          t(j * WData + WData - 1, j * WData) = d(WData - 1, 0);
+        }
+        _blocks[nb][i] = t;
+      }
+    }
+  } else {
+    for (int i = 0; i < 4096; i++) {
+#pragma HLS PIPELINE II = 1
+      for (int nb = 0; nb < _num_uram_block; nb++) {
+        int offset = nb % _num_one_process;
+        int begin = offset * 72;
+        if (offset == (_num_one_process - 1)) {
+          ap_uint<72> t = d(WData - 1, begin);
+          _blocks[nb][i] = t;
+        } else
+          _blocks[nb][i] = d(begin + 71, begin);
+      }
+    }
+  }
+  // update d for cache
+  if (WData <= 72)
+    _index[0] = _elem_per_line * _num_uram_block * 4096 - 1;
+  else
+    _index[0] = _num_uram_block / _num_one_process * 4096 - 1;
+  _state[0] = d;
+init_cache:
+  for (int i = 1; i < NCache; i++) {
+#pragma HLS unroll
+    _state[i] = d;
+    _index[i] = _index[i - 1] - 1;
+  }
+
+  return _num_uram_block;
+}
+
+template <int WData, int NData, int NCache>
 void uram_array<WData, NData, NCache>::write(int index,
-                                             const ap_uint<WData> &d) {
+                                             const ap_uint<WData>& d) {
 #pragma HLS inline
   int div_block = 0, div_index = 0;
   int dec_block = 0, dec, begin;
-
-#pragma HLS ARRAY_PARTITION variable = _blocks complete dim = 1
-#pragma HLS ARRAY_PARTITION variable = _index complete dim = 1
-#pragma HLS ARRAY_PARTITION variable = _state complete dim = 1
 
 Write_Inner:
   for (int i = 0; i < _num_one_process; i++) {
@@ -150,26 +208,22 @@ Write_Inner:
       dec = dec_block % _elem_per_line;
       begin = dec * WData;
       ap_uint<72> tmp = _blocks[div_block][div_index];
-      tmp(begin + WData - 1, begin) = d;
+      tmp.range(begin + WData - 1, begin) = d;
       _blocks[div_block][div_index] = tmp;
     } else {
       div_block = index / 4096;
       dec_block = index % 4096;
       int ii = i + div_block * _num_one_process;
-
+      begin = i * 72;
       if (i == (_num_one_process - 1))
-        _blocks[ii][dec_block] = d.range(WData - 1, ((i << 6) + (i << 3)));
+        _blocks[ii][dec_block] = d.range(WData - 1, begin);
       else
-        _blocks[ii][dec_block] =
-            d.range(((i << 6) + (i << 3)) + 71, ((i << 6) + (i << 3)));
+        _blocks[ii][dec_block] = d.range(begin + 71, begin);
     }
   }
 
 Write_Cache:
   for (int i = NCache - 1; i >= 1; i--) {
-#pragma HLS DEPENDENCE variable = _state inter false
-#pragma HLS DEPENDENCE variable = _index inter false
-
     _state[i] = _state[i - 1];
     _index[i] = _index[i - 1];
   }
@@ -183,10 +237,6 @@ ap_uint<WData> uram_array<WData, NData, NCache>::read(int index) {
   ap_uint<WData> value;
   int div_block = 0, div_index = 0;
   int dec_block = 0, dec, begin;
-
-#pragma HLS ARRAY_PARTITION variable = _blocks complete dim = 1
-#pragma HLS ARRAY_PARTITION variable = _index complete dim = 1
-#pragma HLS ARRAY_PARTITION variable = _state complete dim = 1
 
 Read_Cache:
   for (int i = 0; i < NCache; i++) {
@@ -210,12 +260,13 @@ Read_Inner:
       div_block = index / 4096;
       dec_block = index % 4096;
       int ii = i + div_block * _num_one_process;
+      begin = i * 72;
 
       if (i == (_num_one_process - 1))
-        value.range(WData - 1, ((i << 6) + (i << 3))) = _blocks[ii][dec_block];
-      else
-        value.range(((i << 6) + (i << 3)) + 71, ((i << 6) + (i << 3))) =
-            _blocks[ii][dec_block];
+        value.range(WData - 1, begin) = _blocks[ii][dec_block];
+      else {
+        value.range(begin + 71, begin) = _blocks[ii][dec_block];
+      }
     }
   }
 
--- l1_module/tests/uram_array/test.cpp
+++ l1_module/tests/uram_array/test.cpp
@@ -8,19 +8,16 @@
 
 // as reference uram size 4K*256
 #define WDATA (64)
-#define NDATA (4 << 10)
+#define NDATA (1 << 10)
 #define NCACHE (4)
 
-#define NUM_SIZE (4 << 10)
+#define NUM_SIZE (1 << 10)
 
-void core_test(hls::stream<ap_uint<WDATA> >& out_stream) {
+void core_test(ap_uint<WDATA> ii, hls::stream<ap_uint<WDATA> >& out_stream) {
   xf::util::level1::uram_array<WDATA, NDATA, NCACHE> uram_array1;
 
 l_init_value:
-  for (int i = 0; i < NDATA; ++i) {
-#pragma HLS PIPELINE II = 1
-    uram_array1.write(i, 0);
-  }
+  int num = uram_array1.calloc_uram(ii);
 
 l_read_after_write_test:
   for (int i = 0; i < NUM_SIZE; i++) {
@@ -29,19 +26,19 @@ l_read_after_write_test:
     if ((i & 1) == 0) {
       uram_array1.write(i, i);
     } else {
-      uram_array1.write(i, i);
       ap_uint<WDATA> t = uram_array1.read(i - 1);
       out_stream.write(t);
     }
   }
 
+// test case need to WData > 36£¬if not Cosim fail
 l_update_value_with_1_II:
   for (int i = 0; i < NUM_SIZE; i++) {
 #pragma HLS PIPELINE II = 1
-#pragma HLS DEPENDENCE variable = uram_array1._blocks intra false
+#pragma HLS DEPENDENCE variable = uram_array1._blocks inter false
     ap_uint<WDATA> t = uram_array1.read(i);
     ap_uint<WDATA> u = (t & 1) ? 1 : 0;
-    uram_array1.write(i, u + i);
+    uram_array1.write(i, u);
   }
 
 l_dump_value:
@@ -58,13 +55,12 @@ int uram_array_test() {
   hls::stream<ap_uint<WDATA> > ref_stream("reference");
   ap_uint<WDATA> ref_array[NDATA];
   for (int i = 0; i < NDATA; ++i) {
-    ref_array[i] = 0;
+    ref_array[i] = 1;
   }
   for (int i = 0; i < NUM_SIZE; ++i) {
     if ((i & 1) == 0) {
       ref_array[i] = i;
     } else {
-      ref_array[i] = i;
       ap_uint<WDATA> t = ref_array[i - 1];
       ref_stream.write(t);
     }
@@ -72,19 +68,21 @@ int uram_array_test() {
   for (int i = 0; i < NUM_SIZE; i++) {
     ap_uint<WDATA> t = ref_array[i];
     ap_uint<WDATA> u = (t & 1) ? 1 : 0;
-    ref_array[i] = u + i;
+    ref_array[i] = u;
   }
   for (int i = 0; i < NDATA; ++i) {
     ap_uint<WDATA> t = ref_array[i];
     ref_stream.write(t);
   }
 
+  ap_uint<WDATA> ii = 1;
   hls::stream<ap_uint<WDATA> > out_stream("output");
-  core_test(out_stream);
+  core_test(ii, out_stream);
 
   while (true) {
     ap_uint<WDATA> r = ref_stream.read();
     ap_uint<WDATA> o = out_stream.read();
+    std::cout << r << "  " << o << std::endl;
     if (r != o) {
       if (!nerror)
         std::cout << "The data is incorrect, check implementation."
