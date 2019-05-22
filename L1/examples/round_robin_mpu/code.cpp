032af47ae822558af7b9f3b5d2813c90365eafd8
Yuanqiang Li <yuanqian@xilinx.com>
2019-05-22 00:50:57 -0600

    rename the widths of input and output

--- L1/examples/round_robin_mpu/code.cpp
+++ L1/examples/round_robin_mpu/code.cpp
@@ -6,54 +6,54 @@
 
 #include "code.h"
 
-
-ap_uint<WOUT_STRM> compute(int d) {
-   ap_uint<WOUT_STRM> nd = d;
+// double input as output 
+ap_uint<W_PU> compute(int d) {
+   ap_uint<W_PU> nd = d;
    nd = nd <<1;
    return nd;
 };
 
 int test() {
 
-   hls::stream<ap_uint<WIN_STRM> > data_istrm;
-   hls::stream<bool> e_istrm;
-   hls::stream<ap_uint<WIN_STRM> > ostrm;
-   hls::stream<bool> e_ostrm;
-   
-   int tempa= WIN_STRM*NS/WOUT_STRM;
-   int tempb= tempa*WOUT_STRM;
-   int comp_count= tempb/WIN_STRM; 
+  hls::stream<ap_uint<W_STRM> > istrm;
+  hls::stream<bool> e_istrm;
+  hls::stream<ap_uint<W_STRM> > ostrm;
+  hls::stream<bool> e_ostrm;
+  
+  int tempa= W_STRM*NS/W_PU;
+  int tempb= tempa*W_PU;
+  int comp_count= tempb/W_STRM; 
 
-  std::cout<<std::dec<< "WIN_STRM  = "<< WIN_STRM <<std::endl;
-  std::cout<<std::dec<< "WOUT_STRM = "<< WOUT_STRM <<std::endl;
-  std::cout<<std::dec<< "NSTRM     = "<< NSTRM <<std::endl;
+  std::cout<<std::dec<< "W_STRM  = "<< W_STRM <<std::endl;
+  std::cout<<std::dec<< "W_PU = "<< W_PU <<std::endl;
+  std::cout<<std::dec<< "NPU     = "<< NPU <<std::endl;
   std::cout<<std::dec<< "NS        = "<< NS <<std::endl;
   for(int d=0; d< NS; ++d)  { 
-    data_istrm.write(d);
+    istrm.write(d);
     e_istrm.write(false);
   } 
   e_istrm.write(true);
 
-  round_robin_mpu( data_istrm, e_istrm,
-              ostrm, e_ostrm);
+  round_robin_mpu( istrm, e_istrm,
+                   ostrm, e_ostrm);
 
   int nerror=0;
   int count=0;
   bool first=true;
   
-  while(!e_ostrm.read())  {
-    ap_uint<WIN_STRM> d = ostrm.read(); 
-      ap_uint<WOUT_STRM> gld = compute(count);
+  while(!e_ostrm.read()) {
+    ap_uint<W_STRM> d = ostrm.read(); 
+      ap_uint<W_PU> gld = compute(count);
       if (count <= comp_count && d != gld) {
         nerror=1;
-        std::cout<< "erro: "<<"c="<<count<<", gld="<< gld <<", "<< "current data="<<d <<std::endl;
+        std::cout<< "erro: "<<"c="<<count<<", gld="<< gld <<", "<< " data="<<d <<std::endl;
        } 
      count++;
-  }
-  std::cout << "\n read: " << count <<std::endl;
+  } // while
+  std::cout << "\n total read: " << count <<std::endl;
   if( count != comp_count) {
     nerror=1;
-    std::cout << "\n error  read: " << count <<std::endl;
+    std::cout << "\n error:  total read = " << count << ", comp_count = " << comp_count<<std::endl;
   }
   if (nerror) {
      std::cout << "\nFAIL: " << nerror << "the order is wrong.\n";
--- L1/examples/round_robin_mpu/code.h
+++ L1/examples/round_robin_mpu/code.h
@@ -2,55 +2,50 @@
 #include "xf_utils_hw/stream_one_to_n.h"
 #include "xf_utils_hw/stream_n_to_one.h"
 
-#define WIN_STRM  512 
-#define WOUT_STRM 16
-#define NS       (1024*2*2)
-#define NSTRM    32
+#define W_STRM  512 
+#define W_PU    64
+#define NS      (8*1024)
+#define NPU     8
 
-// a duplicate of input stream, left shift each data 
+// a duplicate of input stream, double each data as output
 void process_core(
-                   hls::stream<ap_uint<WOUT_STRM> >& c_istrm,
+                   hls::stream<ap_uint<W_PU> >& c_istrm,
                    hls::stream<bool>& e_c_istrm,
-                   hls::stream<ap_uint<WOUT_STRM> >& c_ostrm,
+                   hls::stream<ap_uint<W_PU> >& c_ostrm,
                    hls::stream<bool>& e_c_ostrm)
 {
-
-    bool last= e_c_istrm.read(); 
-    while(!last) {
-      #pragma HLS pipeline II=1
-              ap_uint<WOUT_STRM> d    = c_istrm.read();
-              ap_uint<WOUT_STRM> od = d<<1  ;
-              c_ostrm.write(od);
-              e_c_ostrm.write(false);
-              last = e_c_istrm.read();
-     } //while
- 
-     e_c_ostrm.write(true);
-
+  bool last= e_c_istrm.read(); 
+  while(!last) {
+#pragma HLS pipeline II=1
+            ap_uint<W_PU> d  = c_istrm.read();
+            ap_uint<W_PU> od = d<<1;
+            c_ostrm.write(od);
+            e_c_ostrm.write(false);
+            last = e_c_istrm.read();
+   } //while
+   e_c_ostrm.write(true);
 }
-
+// NPU PUs work in parallel
 void  process_mpu( 
-                   hls::stream<ap_uint<WOUT_STRM> > c_istrms[NSTRM],
-                   hls::stream<bool> e_c_istrms[NSTRM],
-                   hls::stream<ap_uint<WOUT_STRM> > c_ostrms[NSTRM],
-                   hls::stream<bool> e_c_ostrms[NSTRM])
+                   hls::stream<ap_uint<W_PU> > c_istrms[NPU],
+                   hls::stream<bool> e_c_istrms[NPU],
+                   hls::stream<ap_uint<W_PU> > c_ostrms[NPU],
+                   hls::stream<bool> e_c_ostrms[NPU])
 {
-
-   #pragma dataflow
-   for( int i=0; i< NSTRM; ++i) {
-      #pragma HLS unroll
-       process_core (
-                     c_istrms[i],
-                     e_c_istrms[i],
-                     c_ostrms[i],
-                     e_c_ostrms[i]);
-
-   }
+#pragma dataflow
+ for( int i=0; i< NPU; ++i) {
+     #pragma HLS unroll
+      process_core (
+                    c_istrms[i],
+                    e_c_istrms[i],
+                    c_ostrms[i],
+                    e_c_ostrms[i]);
+  }
 }
 
-void round_robin_mpu(hls::stream<ap_uint<WIN_STRM> >& istrm,
+void round_robin_mpu(hls::stream<ap_uint<W_STRM> >& istrm,
                    hls::stream<bool>& e_istrm,
-                   hls::stream<ap_uint<WIN_STRM> >& ostrm,
+                   hls::stream<ap_uint<W_STRM> >& ostrm,
                    hls::stream<bool>& e_ostrm) {
 /*
  * One input stream(istrm) is splitted to multitple streams, and each services a PU.
@@ -84,27 +79,27 @@ void round_robin_mpu(hls::stream<ap_uint<WIN_STRM> >& istrm,
  */
 
 #pragma HLS dataflow
-     hls::stream<ap_uint<WOUT_STRM> > data_inner_strms[NSTRM];
+     hls::stream<ap_uint<W_PU> > data_inner_strms[NPU];
 #pragma HLS stream variable = data_inner_strms depth = 8              
-     hls::stream<bool> e_data_inner_strms[NSTRM];
+     hls::stream<bool> e_data_inner_strms[NPU];
 #pragma HLS stream variable = e_data_inner_strms depth = 8              
 
-     hls::stream<ap_uint<WOUT_STRM> > new_data_strms[NSTRM];
+     hls::stream<ap_uint<W_PU> > new_data_strms[NPU];
 #pragma HLS stream variable = new_data_strms depth = 8              
-     hls::stream<bool> e_new_data_strms[NSTRM];
+     hls::stream<bool> e_new_data_strms[NPU];
 #pragma HLS stream variable = e_new_data_strms depth = 8              
  
- xf::common::utils_hw::stream_one_to_n<WIN_STRM, WOUT_STRM,NSTRM>(
+ xf::common::utils_hw::stream_one_to_n<W_STRM, W_PU,NPU>(
                          istrm,  e_istrm,
                          data_inner_strms, e_data_inner_strms,
                          xf::common::utils_hw::round_robin_t());
                        
 
-  process_mpu( data_inner_strms, e_data_inner_strms,
+ process_mpu( data_inner_strms, e_data_inner_strms,
                 new_data_strms,   e_new_data_strms);
 
  
-  xf::common::utils_hw::stream_n_to_one<WOUT_STRM, WIN_STRM,NSTRM>(
+ xf::common::utils_hw::stream_n_to_one<W_PU, W_STRM,NPU>(
                         new_data_strms, e_new_data_strms,
                         ostrm, e_ostrm,
                         xf::common::utils_hw::round_robin_t());
