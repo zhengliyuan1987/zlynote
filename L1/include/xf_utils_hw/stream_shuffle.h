a93af84f55ed9fb0d2c879d68b46fb184629f109
qianqiao <qianqiao@xilinx.com>
2019-06-03 13:35:26 +0800

    fill zero at shuffle output

--- L1/include/xf_utils_hw/stream_shuffle.h
+++ L1/include/xf_utils_hw/stream_shuffle.h
@@ -21,18 +21,22 @@ namespace utils_hw {
  * @brief Shuffle the contents from an array of streams to another.
  *
  * Suppose we have an array of 3 streams for R-G-B channels correspondingly,
- * and it is needed to shuffle B to Stream 0 and R to Stream 2.
- * This module can bridge this case with the configuration ``2, 1, 0``.
- * Here, ``2`` is the new index for data at old stream index ``0``,
- * and ``0`` is the new index for data at old stream index ``2``.
+ * and it is needed to shuffle B to Stream 0, R to Stream 1 and G to Stream 2.
+ * This module can bridge this case with the configuration ``2, 0, 1``.
+ * Here, ``2`` is the source index for data B at destination index ``0``,
+ * and ``0`` is the source index for data R at destination index ``1``,
+ * and ``1`` is the source index for data G at destination index ``2``.
  *
  * The configuration is load once in one invocation, and reused until the end.
  * This module supports up to 128 input streams, and works efficiently within
  * 16.
  *
- * @tparam _INStrm number of input  stream. The advice value is 16 or less.
- * @tparam _ONstrm number of output stream. Should be equal or less than
- * _INStrm.
+ * If minus value is used as the source index, the corresponding stream will be filled with zero.
+ *
+ * If a source index is specified twice, the behavior is undefined.
+ *
+ * @tparam _INStrm number of input  stream.
+ * @tparam _ONstrm number of output stream.
  * @tparam _TIn input type.
  *
  * @param order_cfg the new order within the window. Each 8bit specifies the
@@ -69,13 +73,11 @@ void stream_shuffle(hls::stream<ap_uint<8 * _ONstrm> >& order_cfg,
 
                     hls::stream<_TIn> ostrms[_ONstrm],
                     hls::stream<bool>& e_ostrm) {
-  XF_UTILS_HW_STATIC_ASSERT(
-      _ONstrm <= _INStrm, "stream_shuffle cannot have more output than input.");
 
   XF_UTILS_HW_STATIC_ASSERT(
       _INStrm <= 128, "stream_shuffle cannot handle more than 128 streams.");
 
-  ap_uint<7> route[_ONstrm];
+  ap_int<8> route[_ONstrm];
 #pragma HLS ARRAY_PARTITION variable = route complete
 
   _TIn reg_i[_INStrm];
@@ -86,7 +88,7 @@ void stream_shuffle(hls::stream<ap_uint<8 * _ONstrm> >& order_cfg,
   ap_uint<8 * _ONstrm> orders = order_cfg.read();
   for (int i = 0; i < _ONstrm; i++) {
 #pragma HLS UNROLL
-    route[i] = orders.range(8 * i + 6, 8 * i);
+    route[i] = orders.range(8 * i + 7, 8 * i);
   }
 
   bool e = e_istrm.read();
@@ -106,7 +108,7 @@ void stream_shuffle(hls::stream<ap_uint<8 * _ONstrm> >& order_cfg,
 
     for (int i = 0; i < _ONstrm; i++) {
 #pragma HLS UNROLL
-      reg_o[i] = reg_i[route[i]]; // critical path
+      if(!route[i][7]) reg_o[i] = reg_i[route[i]]; // critical path
     }
 
     for (int i = 0; i < _ONstrm; i++) {
