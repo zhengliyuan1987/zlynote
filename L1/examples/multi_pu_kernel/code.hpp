be2caf5599b76dd3e4af1ead795809a93d0df35a
Yuanjie Huang <yuanjieh@xilinx.com>
2019-07-07 20:29:33 -0700

    Update license year

--- L1/examples/multi_pu_kernel/code.hpp
+++ L1/examples/multi_pu_kernel/code.hpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "xf_utils_hw/stream_one_to_n.hpp"
 #include "xf_utils_hw/stream_n_to_one.hpp"
--- L1/examples/multi_pu_kernel/run_hls.tcl
+++ L1/examples/multi_pu_kernel/run_hls.tcl
@@ -1,8 +1,18 @@
-############################################################
-## This file is generated automatically by Vivado HLS.
-## Please DO NOT edit it.
-## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
-############################################################
+#
+# Copyright 2019 Xilinx, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 open_project -reset  axi_stream_mpu.prj
 config_debug
 
