a8f639ce5d84ddcd62cec214ded2e7f00c1d86cf
Qianqiao Chen <qianqiao.chen@xilinx.com>
2020-05-08 23:09:08 +0800

    fix cache (#142)

--- L1/tests/cache_ro_1DDR_without_e/cache_tb.hpp
+++ L1/tests/cache_ro_1DDR_without_e/cache_tb.hpp
@@ -7,7 +7,7 @@
 #define BUSADDRWIDTH (32)
 #define BUSDATAWIDTH (64)
 #define EACHLINE (512 / BUSDATAWIDTH)
-#define RAMROW (4096)
+#define RAMROW (32)
 #define GRPRAM (4)
 
 #define TOTALADDRWIDTH (15) // should be smaller for CoSim 15 for example
